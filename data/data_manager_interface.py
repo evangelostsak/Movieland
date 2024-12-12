6b894257fe534294052d58f11693e66a5726a3b8
evangelostsak <baggtsak55@gmail.com>
2024-12-12 22:21:39 +0100

    init commit

---
+++ data/data_manager_interface.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class DataManagerInterface(ABC):
+    """
+    Defines abstract class for DataManagerInterface
+    Will be used to create the DataManager class.
+    """
+
+    @abstractmethod
+    def get_all_users(self):
+        pass
+
+    @abstractmethod
+    def get_user_movies(self, user_id):
+        pass
+
+    @abstractmethod
+    def add_user(self, user):
+        pass
+
+    @abstractmethod
+    def delete_user(self, user_id):
+        pass
+
+    @abstractmethod
+    def update_user(self, user_id):
+        pass
+
+    @abstractmethod
+    def add_movie(self, title, director, year, rating):
+        pass
+
+    @abstractmethod
+    def update_movie(self, movie_id, title, director, year, rating):
+        pass
+
+    @abstractmethod
+    def delete_movie(self, movie_id):
+        pass
