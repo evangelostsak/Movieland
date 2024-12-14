993cd94aaf3d18dadd9baa645da509e838adb6e5
evangelostsak <baggtsak55@gmail.com>
2024-12-14 22:46:23 +0100

    data_models.py extended

--- data_manager/data_models.py
+++ data_manager/data_models.py
@@ -19,9 +19,11 @@ class User(db.Model):
     user_movies = relationship('UserMovie', backref='user', cascade='all, delete', lazy=True)
 
     def __str__(self):
+        """ Human-readable presentation of the model user"""
         return f"{self.id}. {self.name}"
 
     def __repr__(self):
+        """Returns a string representation of the User model, debugging-friendly"""
         return f"User(id = {self.id}, name = {self.name})"
 
 
@@ -42,9 +44,11 @@ class Movie(db.Model):
     user_movies = relationship('UserMovie', backref='movie', cascade='all, delete', lazy=True)
 
     def __str__(self):
+        """Human-readable string representation of the model Movie"""
         return f"{self.id}. {self.title}, {self.release_year}"
 
     def __repr__(self):
+        """Returns a string representation of the Movie model, debugging-friendly"""
         return (f"Movie(id = {self.id}, title = {self.title}, release_year = {self.release_year}, "
                 f"poster = {self.poster}, director = {self.director}, rating = {self.rating})")
 
@@ -60,4 +64,5 @@ class UserMovie(db.Model):
     movie_id = Column(Integer, ForeignKey('movie.id'), nullable=False)
 
     def __repr__(self):
+        """Returns a string representation of the UserMovie model, debugging-friendly"""
         return f"UserMovie(id = {self.id}, user_id = {self.user_id}, movie_id {self.movie_id})"
