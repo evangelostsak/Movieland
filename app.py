20a2d415fd71756f64b29501f646be3045877690
evangelostsak <baggtsak55@gmail.com>
2024-12-15 01:51:42 +0100

    home route

--- app.py
+++ app.py
@@ -22,3 +22,12 @@ data = SQLiteDataManager(app)
 #     data.db.create_all()
 
 
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
+
+
