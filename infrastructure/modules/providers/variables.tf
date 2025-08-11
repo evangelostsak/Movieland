41094c42e58d93aa46d7c47736a36decd72cc82a
evangelostsak <baggtsak55@gmail.com>
2025-08-11 22:57:13 +0200

    Providers module

---
+++ infrastructure/modules/providers/variables.tf
@@ -0,0 +1,9 @@
+variable "profile" {
+    description = "AWS profile to use for authentication"
+    type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
