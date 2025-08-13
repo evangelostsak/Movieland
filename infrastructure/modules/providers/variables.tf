34ada1f63bcd401ada0330809dae66fdb3e5605f
evangelostsak <baggtsak55@gmail.com>
2025-08-13 23:58:35 +0200

    providers module deleted

--- infrastructure/modules/providers/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "profile" {
-    description = "AWS profile to use for authentication"
-    type        = string
-}
-
-variable "region" {
-  description = "AWS region to deploy resources"
-  type        = string
-}
