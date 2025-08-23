4160283e26904a98da5cb5dcab01cdeaa0170438
evangelostsak <baggtsak55@gmail.com>
2025-08-24 00:42:40 +0200

    commented out database monitoring, not yet implemented, moved iam related configs to IAM module

--- infrastructure/modules/database/main.tf
+++ infrastructure/modules/database/main.tf
@@ -9,8 +9,8 @@ resource "aws_db_instance" "db_instance" {
   password            = var.db_pass
   skip_final_snapshot = true
   backup_retention_period = 7
-  monitoring_interval = 60  # Enhanced monitoring every 60s
-  monitoring_role_arn = aws_iam_role.rds_monitoring.arn
+  # monitoring_interval = 60  # Enhanced monitoring every 60s        # Not yet
+  # monitoring_role_arn = aws_iam_role.rds_monitoring.arn
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
   publicly_accessible  = false
  vpc_security_group_ids = var.vpc_security_group_ids
@@ -37,25 +37,4 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   tags = {
     Name = "movieland-db-subnet-group"
   }
-}
-
-
-# IAM Role for RDS Monitoring
-resource "aws_iam_role" "rds_monitoring" {
-  name = "rds-monitoring-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "monitoring.rds.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-# Attach RDS monitoring policy to the role
-resource "aws_iam_role_policy_attachment" "rds_monitoring_policy" {
-  role       = aws_iam_role.rds_monitoring.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
 }
