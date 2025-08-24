f11315ffce603e49465e3daa73a8c64a533445ea
evangelostsak <baggtsak55@gmail.com>
2025-08-24 11:55:30 +0200

    moved iam configurations to IAM module

--- infrastructure/modules/storage/main.tf
+++ infrastructure/modules/storage/main.tf
@@ -19,52 +19,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_crypto_con
   }
 }
 
-# IAM Role & Policy
-resource "aws_iam_role" "ec2_s3_role" {
-  name = "s3_access_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "s3_access_policy"
-  description = "Allow EC2 to access specific S3"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
-        Resource = "${aws_s3_bucket.bucket.arn}/*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
-  role       = aws_iam_role.ec2_s3_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "ec2-s3-instance-profile"
-  role = aws_iam_role.ec2_s3_role.name
-}
-
 # S3 Bucket Policy
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
@@ -74,7 +28,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       {
         Effect = "Allow",
         Principal = {
-          AWS = aws_iam_role.ec2_s3_role.arn
+          AWS = var.ec2_s3_role_arn
         },
         Action = [
           "s3:GetObject",
--- infrastructure/modules/storage/variables.tf
+++ infrastructure/modules/storage/variables.tf
@@ -3,3 +3,8 @@ variable "bucket_prefix" {
   description = "prefix of s3 bucket for app data"
   type        = string
 }
+
+variable "ec2_s3_role_arn" {
+  type = string
+  description = "ARN of the EC2 IAM role for S3 access"
+}
